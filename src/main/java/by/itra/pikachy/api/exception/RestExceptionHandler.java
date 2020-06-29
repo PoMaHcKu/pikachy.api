6586555c3662605e5d0d3fb4ca1cb1f3cdfe4291
r.popov <rom2517pop@gmail.com>
2020-06-29 17:22:42 +0300

    Update exception handle - 6

--- src/main/java/by/itra/pikachy/api/exception/RestExceptionHandler.java
+++ src/main/java/by/itra/pikachy/api/exception/RestExceptionHandler.java
@@ -3,7 +3,6 @@ package by.itra.pikachy.api.exception;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
