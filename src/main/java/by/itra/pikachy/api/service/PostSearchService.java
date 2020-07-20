7ea2bb042d2ced1c57d847b660949bbbf65ce14c
pomahcku <rom2517pop@gmail.com>
2020-07-20 13:13:31 +0300

    creating indexes at heroku by request.

--- src/main/java/by/itra/pikachy/api/service/PostSearchService.java
+++ src/main/java/by/itra/pikachy/api/service/PostSearchService.java
@@ -80,12 +80,4 @@ public class PostSearchService {
         FullTextQuery jpaQuery = manager.createFullTextQuery(query, Post.class, Section.class);
         return jpaQuery.getResultList();
     }
-
-    public void updateIndexed() {
-        try {
-            fullTextEntityManager.createIndexer().startAndWait();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
