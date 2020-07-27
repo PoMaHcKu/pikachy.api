8889b5faa232b5ff934933fb6a0fcdaae7b18c80
pomahcku <rom2517pop@gmail.com>
2020-07-27 23:48:16 +0300

    Created JwtFilter, JwtProvider

---
+++ src/main/java/by/itra/pikachy/api/security/jwt/JwtProvider.java
@@ -0,0 +1,41 @@
+package by.itra.pikachy.api.security.jwt;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    public String generateToken(String username) {
+        Date date = Date.from(LocalDate.now().plusDays(3).atStartOfDay(ZoneId.systemDefault()).toInstant());
+        return Jwts.builder()
+                .setSubject(username)
+                .setExpiration(date)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException | UnsupportedJwtException |
+                MalformedJwtException | SignatureException expEx) {
+            expEx.printStackTrace();
+        }
+        return false;
+    }
+
+    public String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+        return claims.getSubject();
+    }
+}
